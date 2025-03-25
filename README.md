5eca2566539228d4cf293d99839e34f22b5035cb
Tejas Naik <tejasnaik@Tejass-MacBook-Pro.local>
2025-03-24 21:02:13 -0400

    starting  repository

---
+++ README.md
@@ -0,0 +1,5 @@
+# HYPERNOVA STRIKE
+
+##### Check out the game here!: 
+
+HyperNova Strike is a fun action packed game where you need to 
---
+++ mechanics.js
@@ -0,0 +1,8 @@
+let titleSize = 32;
+let rows = 16;
+let columns = 16;
+
+let board;
+let boardWidth = titleSize * columns; // 32 * 16
+let boardHeight = titleSize * rows; // 32 * 16
+let context;
