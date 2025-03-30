0efe66301c8db39913c57766617d961410828dcc
Tejas Naik <tejasnaik@Tejass-MacBook-Pro.local>
2025-03-29 21:32:10 -0400

    added starting page of website

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 
 ##### Check out the game here!: 
 
-HyperNova Strike is a fun action packed game where you need to 
+HyperNova Strike is a fun action packed game where you need to destroy enemy ships that are coming to destory 
--- mechanics.js
+++ mechanics.js
@@ -1,8 +1,9 @@
-let titleSize = 32;
-let rows = 16;
-let columns = 16;
 
-let board;
-let boardWidth = titleSize * columns; // 32 * 16
-let boardHeight = titleSize * rows; // 32 * 16
-let context;
+
+
+
+
+//function playInstructions()
+
+
+// function startGame()  
