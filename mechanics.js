5ffa7bafff3231af0a8d2f9ba61d5ccec5268459
Tejas Naik <tejasnaik@Tejass-MacBook-Pro.local>
2025-04-02 21:14:11 -0400

    how to play page layout finished

--- mechanics.js
+++ mechanics.js
@@ -1,9 +1,74 @@
 
 
 
+// These next 7 functions contorl the how-to-play page
+
+function playInstructions() {
+    document.getElementById("Instructions").style.display = "none";
+    document.getElementById("ControlsSuperWeapon").style.display = "block";
+    document.getElementById("HealthEnemyProperties").style.display = "block";
+    document.getElementById("BossProperties").style.display = "block";
+    document.getElementById("GameScaling").style.display = "block";
+    document.getElementById("Back").style.display = "block";
+}
+
+function ControlsSuperWeapon() {
+    document.getElementById("ControlsSuperWeapon").style.display = "none";
+    document.getElementById("HealthEnemyProperties").style.display = "none";
+    document.getElementById("BossProperties").style.display = "none";
+    document.getElementById("GameScaling").style.display = "none";
+    document.getElementById("Back").style.display = "none";
+    document.getElementById("goBackToHowtoplay").style.display = "block";
+}
+
+function HealthEnemyProperties() {
+    document.getElementById("ControlsSuperWeapon").style.display = "none";
+    document.getElementById("HealthEnemyProperties").style.display = "none";
+    document.getElementById("BossProperties").style.display = "none";
+    document.getElementById("GameScaling").style.display = "none";
+    document.getElementById("Back").style.display = "none";
+    document.getElementById("goBackToHowtoplay").style.display = "block";
+}
+
+function BossProperties() {
+    document.getElementById("ControlsSuperWeapon").style.display = "none";
+    document.getElementById("HealthEnemyProperties").style.display = "none";
+    document.getElementById("BossProperties").style.display = "none";
+    document.getElementById("GameScaling").style.display = "none";
+    document.getElementById("Back").style.display = "none";
+    document.getElementById("goBackToHowtoplay").style.display = "block";
+}
+
+function GameScaling() {
+    document.getElementById("ControlsSuperWeapon").style.display = "none";
+    document.getElementById("HealthEnemyProperties").style.display = "none";
+    document.getElementById("BossProperties").style.display = "none";
+    document.getElementById("GameScaling").style.display = "none";
+    document.getElementById("Back").style.display = "none";
+    document.getElementById("goBackToHowtoplay").style.display = "block";
+}
+
+function goBackToInstructions() {
+    document.getElementById("Instructions").style.display = "block";
+    document.getElementById("ControlsSuperWeapon").style.display = "none";
+    document.getElementById("HealthEnemyProperties").style.display = "none";
+    document.getElementById("BossProperties").style.display = "none";
+    document.getElementById("GameScaling").style.display = "none";
+    document.getElementById("Back").style.display = "none";
+}
+
+function goBackToHowtoplay() {
+    document.getElementById("ControlsSuperWeapon").style.display = "block";
+    document.getElementById("HealthEnemyProperties").style.display = "block";
+    document.getElementById("BossProperties").style.display = "block";
+    document.getElementById("GameScaling").style.display = "block";
+    document.getElementById("Back").style.display = "block";
+    document.getElementById("goBackToHowtoplay").style.display = "none";
+}
 
 
-//function playInstructions()
+
+
 
 
 // function startGame()  
